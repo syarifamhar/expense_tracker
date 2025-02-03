6d84a1fa9f9ddb91a059ce7783fd2da9611d23ac
Syarif Amhar <syarifamhar@gmail.com>
2025-02-03 10:43:40 +0700

    Fix build.gradle and added package fl_charts

---
+++ lib/screens/stats/stats.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class StatScreen extends StatelessWidget {
+  const StatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        child: Column(
+          children: [
+            const Text(
+              "Transactions Test",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              color: Colors.red,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
